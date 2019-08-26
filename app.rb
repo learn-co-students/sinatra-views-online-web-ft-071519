87f6d85e17aee823a1c54f3e7fc4ad4779e8309b
Audrea Cook <audreacwork@gmail.com>
2019-08-26 14:27:39 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
 
 	get '/' do
-		
+	  erb :index
 	end
+
+	get '/info' do
+	  erb :info
+	end
+
 end
